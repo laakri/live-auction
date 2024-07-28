c71105a613dbdb627f38febee54ebdaf57eed7c7
Jelassi Saif <68915309+laakri@users.noreply.github.com>
2024-07-28 13:00:23 +0100

    update the ui again

--- client/src/pages/AuctionGuides/index.tsx
+++ client/src/pages/AuctionGuides/index.tsx
@@ -24,7 +24,7 @@ const AuctionGuides = () => {
   ];
 
   return (
-    <div className=" mx-auto px-4 py-8  text-gray-100 ">
+    <div className="container mx-auto px-4 py-8  text-gray-100 ">
       <h1 className="text-3xl font-bold mb-4 text-center ">Auction Guides</h1>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
         {guides.map((guide, index) => (
